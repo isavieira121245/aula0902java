732b538cf1f236009ffcdf24888ef02d18ea0540
isavieira121245 <visadora178@gmail.com>
2024-02-23 11:15:23 +0000

    salvo

--- app/src/main/java/primeiro_exemplo/hello.java
+++ app/src/main/java/primeiro_exemplo/hello.java
@@ -1,13 +1,38 @@
 package primeiro_exemplo;
-
+ 
+import primeiro_exemplo.modelos.Pessoa;
+ 
 public class hello {
-      public static void main(String[] args) {
-          System.out.println("Olá Mundo!!!");
-
-
-
-      }
-
-
-
+    public static void main(String[] args) {
+        int cont = 16022024;
+        String nome = "Etec";
+        System.out.println("Bom Dia!!!");
+ 
+        System.out.println(cont + " : " + nome);
+       
+        Pessoa p1 = new Pessoa();
+ 
+        p1.nome = "isadora";
+        p1.idade = 17 ;
+ 
+        System.out.println(p1.nome + ":" + p1.idade);
+ 
+        Pessoa p2 = new Pessoa();
+        p2.nome = "felipe";
+        p2.idade = 21 ;
+ 
+        System.out.println(p2.nome + ":" + p2.idade);
+     
+        Pessoa p3 = new Pessoa();
+        p3.nome = "Rafaela";
+        p3.idade = 10;
+ 
+        System.out.println(p3.nome + ":" + p3.idade);
+     
+        System.out.println("Se marcar *true* sua você é maior de idade e se der *false* você é menor de idade:");
+ 
+        System.out.println(p1.verificarMaioridade());
+        System.out.println(p2.verificarMaioridade());
+        System.out.println(p3.verificarMaioridade());
+    }
 }
---
+++ app/src/main/java/primeiro_exemplo/modelos/Pessoa.java
@@ -0,0 +1,13 @@
+package primeiro_exemplo.modelos;
+
+public class Pessoa {
+    // Atributos
+    private String nome;
+    private int idade;
+ 
+  //Método
+public boolean verificarMaioridade() {
+return this.idade >= 18;
+ }
+ 
+}
